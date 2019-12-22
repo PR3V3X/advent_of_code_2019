b5394f3d9f307768c01bc99ed268e7388405cc98
PR3V3X <59129974+PR3V3X@users.noreply.github.com>
2019-12-21 21:47:23 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,5 +1,6 @@
 '''
-Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+Fuel required to launch a given module is based on its mass. 
+Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
 '''
 # f = ((m / 3) // 1) - 2
 
